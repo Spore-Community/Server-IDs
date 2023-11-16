124113b1b84e738945f30b1745c5e933748e3fa2
Kade <kade@aceofkade.com>
2023-11-15 23:58:08 -0500

    Add scripts to sort and cleanup IDs

---
+++ cleanup_ids.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Organizes all IDs to ensure they are in-order and contain no duplicates
+# Usage: cleanup_ids.sh path/to/file.txt
+
+sort -u "$1" -o "$1"
---
+++ sort_ids.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Sorts IDs from all txt files in type/type_unsorted into the appropriate location
+# Usage: sort_ids.sh type
+
+# Operate on every file in the folder
+for file in $1/$1_unsorted/*.txt; do
+  echo "Processing IDs in $file"
+  while read line || [[ -n "$line" ]]; do
+    # Skip any lines that are not 12-digit numbers
+    if [[ ! $line =~ ^[0-9]{12}$'\r'?$'\n'?$ ]]; then
+      echo "Skipping line in $file: $line"
+      continue
+    fi
+
+    # Split the ID into sub-IDs
+    first=$(echo "$line" | cut -c1-3)
+    second=$(echo "$line" | cut -c4-6)
+    third=$(echo "$line" | cut -c7-9)
+
+    #echo "$first/$second/$third.txt: $line"
+
+    # Create the directories
+    mkdir -p "$1/$first/$second"
+
+    # Add the line to the appropriate file
+    echo "$line" >> "$1/$first/$second/$third.txt"
+
+    # Sort the file and remove duplicates - this is inefficient, do this afterwards in a separate script
+    #sort -u "$1/$first/$second/$third.txt" -o "$1/$first/$second/$third.txt"
+  done < "$file"
+  rm -I "$file"
+done
